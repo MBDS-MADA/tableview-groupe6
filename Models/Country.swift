0b95458774683b6833054faa5c5cb2d07a24d092
bauda04 <antenainarandrianantoandro@gmail.com>
2023-02-26 00:10:48 -0800

    Initial commit

---
+++ Models/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  TP2_Groupe_6
+//
+//  Created by user224319 on 2/17/23.
+//
+
+import Foundation
+
+struct Country{
+    var isoCode: String
+    var name: String
+    var continent: String
+}
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TP2_Groupe_6
+//
+//  Created by user224319 on 2/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
