a2ff78f33fb4273e04c1dec5b9bbb71987f06a0d
bauda04 <antenainarandrianantoandro@gmail.com>
2023-02-25 23:57:07 -0800

    Initial Commit

---
+++ TP2_Groupe_6/Data/Countries.swift
@@ -0,0 +1,17 @@
+//
+//  Countries.swift
+//  TP2_Groupe_6
+//
+//  Created by user224319 on 2/17/23.
+//
+
+import Foundation
+
+let countries = [
+    Country(isoCode: "at", name: "Austria", continent: "Europe"),
+    Country(isoCode: "be", name: "Belgium", continent: "Europe"),
+    Country(isoCode: "de", name: "Germany", continent: "Europe"),
+    Country(isoCode: "el", name: "Greece", continent: "Europe"),
+    Country(isoCode: "fr", name: "France", continent: "Europe"),
+    Country(isoCode: "mg", name: "Madagascar", continent: "Afrique"),
+    Country(isoCode: "ar", name: "Argentine", continent: "Amerique")]
